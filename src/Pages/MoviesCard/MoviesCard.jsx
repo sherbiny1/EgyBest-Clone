ded7e05ce1082dd8358589db8c33af959610bbaf
sherbiny1 <melsherbiny193@gmail.com>
2025-08-19 18:21:49 +0300

    Initial commit with all edit egybest files

--- src/Pages/MoviesCard/MoviesCard.jsx
+++ src/Pages/MoviesCard/MoviesCard.jsx
@@ -26,9 +26,9 @@ export default function MoviesCard({ themeChange, theme }) {
         {movieCard==null ? <Loading/>: 
         <>
             <Navbar themeChange={themeChange} theme={theme}/>
-            <div className="grid lg:grid-cols-12 px-40 py-5 lg:gap-8">
-                <div className="movie-poster lg:col-span-4">
-                    <img src={`https://image.tmdb.org/t/p/w500${movieCard.poster_path}`} alt="" />
+            <div className="grid lg:grid-cols-12 px-10 lg:px-40 py-5 lg:gap-8">
+                <div className="movie-poster lg:col-span-4 rounded-2xl">
+                    <img className="rounded-2xl" src={`https://image.tmdb.org/t/p/w500${movieCard.poster_path}`} alt="" />
                 </div>
                 <div className="movie-content lg:col-span-8 space-y-3">
                     <h2 className="font-semibold text-2xl">{movieCard.title}</h2>
